e159c998fc82a8d6a1a7de76688300a0f4fb4c6a
airetriton <wobbe_espinoza@hotmail.com>
2017-10-14 21:09:19 -0600

    fifth

--- javascript/game_old.js
+++ javascript/game_old.js
@@ -6,14 +6,6 @@ $(document).ready(function() {
 
 //call your game
 
-});
-
-var minNumber = 19;
-var maxNumber = 120; 
-
-//create random numbers for game number and crystals.
-var randomizer = function getRandomInt(minNumber, maxNumber) {
-    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 };
 
 var gameLife = 0;
@@ -26,39 +18,29 @@ var crystalNumber4 = randomizer (1,12);
 
 //create play
 //create play again
-//create reset without refreshing page
-
 //
- 
+var 
 
 //create randomizer for value for crystals, create attr. to put data into, add data to counter, make sure to show the user guess
 //create var that uses min and max
 
 //when start button is clicked generate random number and show on screen.
-	$("#gameStart").click(function() {
-       
-     $("#randomNumber").html(targetNumber);
-     $("#box1").append(crystalNumber1);
-     $("#box2").append(crystalNumber2);
-     $("#box3").append(crystalNumber3);
-     $("#box4").append(crystalNumber4);
-  
+	$("#gameStart").on("click", function() {
+        
+        var randomNumber = Math.floor(19 + (parseInt)(Math.random() * ((120 - 19) +1)));
+        $('#randomNumber').text(randomNumber);
         
       });
-	console.log(crystalNumber1);
-	console.log(crystalNumber2);
-	console.log(crystalNumber3);
-	console.log(crystalNumber4);
 
-	// $("*#box").on("click", function(){
-	// 	var crystalNumber = Math.floor((Math.random() * 12) +1);
-	// 	$('crystalNumber') 
+	$("*#box").on("click", function(){
+		var crystalNumber = Math.floor((Math.random() * 12) +1);
+		$('crystalNumber') 
 
 
-// 	})
+	})
 
 
-// });
+});
 
 
 // $('button').click(function() {
